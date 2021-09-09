f406a9c79843db51488da434118d6732901d47df
Xavier Baszuk <48223045+baszukxj@users.noreply.github.com>
2021-09-09 14:40:31 -0400

    Update /endpoint

--- src/main/java/com/plantplaces/PlantPlacesController.java
+++ src/main/java/com/plantplaces/PlantPlacesController.java
@@ -3,6 +3,12 @@ package com.plantplaces;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
+/**
+ * Handle the /start endpoint
+ * @return
+ *
+ */
 @Controller
 public class PlantPlacesController {
 	
@@ -11,4 +17,18 @@ public class PlantPlacesController {
 		
 		return "start";
 	}
+	
+	/**
+	 * Handle the endpoint
+	 * @return
+	 *
+	 */
+	
+	@RequestMapping("/")
+	public String index() {
+		
+		return "start";
+	}
+	
+	
 }
